e470b23f17fc35f351aaf4c21418b5871566fc68
Brent Martin <bmart35@gmail.com>
2016-09-26 16:56:41 -0500

    created a handler for the websocket

--- main.go
+++ main.go
@@ -7,14 +7,18 @@ import "net/http"
 // Startup main goroutine
 func main() {
 	// websocket handler
+	http.HandleFunc("/socket", socketHandler)
 	// html handler
 	// start server
 }
 
 // Create handler for websocket
-//      upgrade the connection
-//      put in a reader for messages sent to server
-//      put in a writer for messages printed back from server
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	// upgrade the connection
+	// put in a reader for messages sent to server
+	// put in a writer for messages printed back from server
+}
+
 // Create a handler for the html
 //      write HTML for messages
 //      write JS to send messages
