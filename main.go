14cf2db7a7e496efb5e130fe3534c343288895c3
Brent Martin <bmart35@gmail.com>
2016-09-29 16:07:44 -0500

    cleanup notes

--- main.go
+++ main.go
@@ -8,14 +8,12 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// Initialize a variable that uses a websocket upgrader
 var upgrader = websocket.Upgrader{}
 
-// Startup main goroutine
 func main() {
 	// websocket handler
 	http.HandleFunc("/socket", socketHandler)
-	// html handler
+	// index file handler
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	// start server
 	err := http.ListenAndServe(":8080", nil)
@@ -24,7 +22,6 @@ func main() {
 	}
 }
 
-// Create handler for websocket
 func socketHandler(w http.ResponseWriter, r *http.Request) {
 	// upgrade the connection
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -34,23 +31,19 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for {
-		// put in a reader for messages sent to server
+		// read messages from server
 		message, p, err := conn.ReadMessage()
 		if err != nil {
 			return
 		}
 
+		// timeout between actions
 		time.Sleep(time.Second * 1)
-		// put in a writer for messages printed back from server
+
+		// write messages to server
 		err = conn.WriteMessage(message, p)
 		if err != nil {
 			return
 		}
 	}
 }
-
-// Create a handler for the html
-//      write HTML for messages
-//      write JS to send messages
-//      stylize a bit
-// Start server to serve those handlers
